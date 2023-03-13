f378b6bfd1992b9693cbe441265bfb98cba91ac8
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2023-03-13 22:43:05 +0100

    initial commit

---
+++ src/main/java/cloud/cholewa/joboffers/JobOffersApplication.java
@@ -0,0 +1,13 @@
+package cloud.cholewa.joboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersApplication.class, args);
+    }
+
+}
